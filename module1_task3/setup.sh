3ab1edba258b1d4c07bdfe22078d55308f2e8f49
joana.casallas <joacasallas@gmail.com>
2025-03-27 13:48:57 -0500

    docker

--- module1_task3/setup.sh
+++ module1_task3/setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
-apt-get update
+echo "Updating package list..."
+apt-get update || true
 echo "Installing Hugo and Make..."
-apt-get install --yes hugo=0.92* make=4*
+apt-get install --yes hugo=0.92* make=4* || {
+    echo "Failed to install or Make. Retrying ..."
+}
 echo "Running 'make build'..."
 make build
