f52bd32b73b3a46f247ce8446fd4892fb28329e6
joana.casallas <joacasallas@gmail.com>
2025-04-03 11:53:26 -0500

    docker to get the error from production

--- module1_task3/setup.sh
+++ module1_task3/setup.sh
@@ -5,15 +5,22 @@ set -e
 
 # Update package list
 echo "Updating package list..."
-apt-get update
+apt-get update || true
 
 ## Install Hugo
-apt-get install --yes hugo=0.92*
+echo "Installing hugo..."
+apt-get install --yes hugo=0.92* || {
+    echo "Failed to install Hugo. Retrying..."
+    apt-get install --yes hugo=0.92*
+}
 
 ## Install Make
-apt-get install --yes make=4*
+echo "Installing make..."
+apt-get install --yes make=4* || {
+    echo "Failed to install Make. Retrying..."
+    apt-get install --yes make=4*
+}
 
 # Run make build to reproduce the error
 echo "Running 'make build'..."
 make build
-
